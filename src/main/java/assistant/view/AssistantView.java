8c70584e8216740b016dca7c9a7f5aab68a1c70c
Zane Stockton <zane.stockton@hotmail.com>
2019-02-27 19:42:58 -0700

    Update #3
    - Removed a useless class
    + linked API to the GUI

--- src/main/java/assistant/view/AssistantView.java
+++ src/main/java/assistant/view/AssistantView.java
@@ -1,5 +1,32 @@
 package assistant.view;
 
-public class AssistantView {
+import javafx.stage.Stage;
+import mtgassistant.view.ApiAssistantView;
+
+public class AssistantView implements ApiAssistantView {
+
+	public Stage stage() {
+		return null;
+	}
+
+	public void init() {
+		
+	}
+
+	public void drawPlayers() {
+		
+	}
+
+	public void drawDecks() {
+		
+	}
+
+	public void drawTokens() {
+		
+	}
+
+	public void drawCounters() {
+		
+	}
 	
 }
