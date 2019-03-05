91103fc383c95470632d8c71cc50859c24e427b0
Zane Stockton <zane.stockton@hotmail.com>
2019-03-05 09:17:04 -0700

    Removed useless imports

--- src/main/java/assistant/MtgAssistant.java
+++ src/main/java/assistant/MtgAssistant.java
@@ -5,10 +5,8 @@ import java.net.URL;
 import assistant.view.AssistantView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import mtgassistant.controller.AssistantController;
 
